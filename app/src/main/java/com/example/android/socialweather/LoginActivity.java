e33ba37b43dfc7178b1ca25a7701ccc06530b380
ryubears <ryubears@gmail.com>
2017-07-07 22:15:33 +0900

    Added Facebook Login and its Design

--- app/src/main/java/com/example/android/socialweather/LoginActivity.java
+++ app/src/main/java/com/example/android/socialweather/LoginActivity.java
@@ -6,12 +6,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.accountkit.AccessToken;
 import com.facebook.accountkit.AccountKit;
 import com.facebook.accountkit.AccountKitLoginResult;
 import com.facebook.accountkit.ui.AccountKitActivity;
 import com.facebook.accountkit.ui.AccountKitConfiguration;
 import com.facebook.accountkit.ui.LoginType;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,28 +26,59 @@ import butterknife.OnClick;
 public class LoginActivity extends AppCompatActivity {
     @BindView(R.id.login_phone_button) Button mPhoneButton;
     @BindView(R.id.login_email_button) Button mEmailButton;
+    @BindView(R.id.login_facebook_button) LoginButton mFacebookButton;
 
     public static int LOGIN_REQUEST_CODE = 1;
+
+    private CallbackManager mCallbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //bind views with butterknife
         ButterKnife.bind(this);
 
-        //check if an accessToken already exists
+        //set facebook login permissions
+        mFacebookButton.setReadPermissions("email");
+
+        //register callback for facebook login
+        mCallbackManager = CallbackManager.Factory.create();
+        mFacebookButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                launchSocialActivity();
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                //display error message
+                String toastMessage = error.getMessage();
+                Toast.makeText(LoginActivity.this, toastMessage, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        //check if an ClientKit/Facebook accessToken already exists
         AccessToken accessToken = AccountKit.getCurrentAccessToken();
-        if(accessToken != null) {
+        com.facebook.AccessToken loginToken = com.facebook.AccessToken.getCurrentAccessToken();
+        if(accessToken != null || loginToken != null) {
             //if previously logged in, proceed to social activity
             launchSocialActivity();
         }
-
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
+        //call facebook callback manager's onActivityResult for facebook login
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
 
         //confirm that response matches request code
         if(requestCode == LOGIN_REQUEST_CODE) {
@@ -76,11 +113,19 @@ public class LoginActivity extends AppCompatActivity {
 
     @OnClick(R.id.login_phone_button)
     public void onPhoneLogin() {
+        //logs phone events
+        AppEventsLogger logger = AppEventsLogger.newLogger(this);
+        logger.logEvent("onPhoneLogin");
+
         onLogin(LoginType.PHONE);
     }
 
     @OnClick(R.id.login_email_button)
     public void onEmailLogin() {
+        //logs email events
+        AppEventsLogger logger = AppEventsLogger.newLogger(this);
+        logger.logEvent("onEmailLogin");
+
         onLogin(LoginType.EMAIL);
     }
 
--- app/src/main/java/com/example/android/socialweather/SocialActivity.java
+++ app/src/main/java/com/example/android/socialweather/SocialActivity.java
@@ -1,20 +1,29 @@
 package com.example.android.socialweather;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
 import com.facebook.accountkit.Account;
 import com.facebook.accountkit.AccountKit;
 import com.facebook.accountkit.AccountKitCallback;
 import com.facebook.accountkit.AccountKitError;
 import com.facebook.accountkit.PhoneNumber;
+import com.facebook.login.LoginManager;
 import com.google.i18n.phonenumbers.NumberParseException;
 import com.google.i18n.phonenumbers.PhoneNumberUtil;
 import com.google.i18n.phonenumbers.Phonenumber;
+import com.makeramen.roundedimageview.RoundedTransformationBuilder;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
 
 import java.util.Locale;
 
@@ -26,47 +35,89 @@ public class SocialActivity extends AppCompatActivity {
     @BindView(R.id.social_id_text_view) TextView mIdTextView;
     @BindView(R.id.social_info_text_view) TextView mInfoTextView;
     @BindView(R.id.social_logout_button) Button mLogoutButton;
+    @BindView(R.id.social_profile_image_view) ImageView mProfileImageView;
+
+    private ProfileTracker mProfileTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social);
 
+        //bind views with butterknife
         ButterKnife.bind(this);
 
-        AccountKit.getCurrentAccount(new AccountKitCallback<Account>() {
+        //tracks changes to profile
+        mProfileTracker = new ProfileTracker() {
             @Override
-            public void onSuccess(Account account) {
-                //get account kit id
-                String accountKitId = account.getId();
-                mIdTextView.setText(accountKitId);
-
-                PhoneNumber phoneNumber = account.getPhoneNumber();
-                if(phoneNumber != null) {
-                    //if phone number is available, display it
-                    String formattedPhoneNumber = formatPhoneNumber(phoneNumber.toString());
-                    mInfoTextView.setText(formattedPhoneNumber);
-                } else {
-                    //if email address is available, display it
-                    String emailString = account.getEmail();
-                    mInfoTextView.setText(emailString);
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
+                if(currentProfile != null) {
+                    //displays the changed profile info
+                    displayProfileInfo(currentProfile);
                 }
             }
+        };
 
-            @Override
-            public void onError(AccountKitError accountKitError) {
-                //display error message
-                String toastMessage = accountKitError.getErrorType().getMessage();
-                Toast.makeText(SocialActivity.this, toastMessage, Toast.LENGTH_LONG).show();
+        if(AccessToken.getCurrentAccessToken() != null) { //if user logged in through facebook
+            //get current profile
+            Profile profile = Profile.getCurrentProfile();
+            if(profile != null) {
+                //displays profile
+                displayProfileInfo(profile);
+            } else {
+                //fetch profile info and calls onCurrentProfileChanged
+                Profile.fetchProfileForCurrentAccessToken();
             }
-        });
+        } else { //if user logged in through account kit
+            AccountKit.getCurrentAccount(new AccountKitCallback<Account>() {
+                @Override
+                public void onSuccess(Account account) {
+                    //get account kit id
+                    String accountKitId = account.getId();
+                    mIdTextView.setText(accountKitId);
+
+                    PhoneNumber phoneNumber = account.getPhoneNumber();
+                    if(phoneNumber != null) {
+                        //if phone number is available, display it
+                        String formattedPhoneNumber = formatPhoneNumber(phoneNumber.toString());
+                        mInfoTextView.setText(formattedPhoneNumber);
+                    } else {
+                        //if email address is available, display it
+                        String emailString = account.getEmail();
+                        mInfoTextView.setText(emailString);
+                    }
+                }
+
+                @Override
+                public void onError(AccountKitError accountKitError) {
+                    //display error message
+                    String toastMessage = accountKitError.getErrorType().getMessage();
+                    Toast.makeText(SocialActivity.this, toastMessage, Toast.LENGTH_LONG).show();
+                }
+            });
+        }
     }
 
     @OnClick(R.id.social_logout_button)
     public void onLogout() {
         //logout from current account
         AccountKit.logOut();
+        LoginManager.getInstance().logOut();
         launchLoginActivity();
+    }
+
+    private void displayProfileInfo(Profile profile) {
+        //set profile id
+        String profileId = profile.getId();
+        mIdTextView.setText(profileId);
+
+        //set user name
+        String name = profile.getName();
+        mInfoTextView.setText(name);
+
+        //set user profile picture
+        Uri profilePicUri = profile.getProfilePictureUri(100, 100);
+        displayProfilePic(profilePicUri);
     }
 
     private void launchLoginActivity() {
@@ -75,7 +126,7 @@ public class SocialActivity extends AppCompatActivity {
         finish();
     }
 
-    //method for formatting phone numbers
+    //helper method for formatting phone numbers
     private String formatPhoneNumber(String phoneNumber) {
         try {
             PhoneNumberUtil pnu = PhoneNumberUtil.getInstance();
@@ -85,5 +136,20 @@ public class SocialActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         return phoneNumber;
+    }
+
+    //helper method for displaying profile pictures
+    private void displayProfilePic(Uri uri) {
+        //transform profile picture in a circular frame
+        Transformation transformation = new RoundedTransformationBuilder()
+                .cornerRadiusDp(30)
+                .oval(false)
+                .build();
+
+        //inserts profile picture to profile image view
+        Picasso.with(SocialActivity.this)
+                .load(uri)
+                .transform(transformation)
+                .into(mProfileImageView);
     }
 }
