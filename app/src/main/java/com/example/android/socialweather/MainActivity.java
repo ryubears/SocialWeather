97bd629a58d3976c56e541905d295d8cd0db3296
ryubears <ryubears@gmail.com>
2017-07-14 00:26:58 +0900

    Changed LogOut Navigation Item Text Color

--- app/src/main/java/com/example/android/socialweather/MainActivity.java
+++ app/src/main/java/com/example/android/socialweather/MainActivity.java
@@ -5,14 +5,16 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.*;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.SpannableString;
 import android.text.TextUtils;
+import android.text.style.ForegroundColorSpan;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
@@ -314,6 +316,12 @@ public class MainActivity extends AppCompatActivity {
 
         //necessary for hamburger icon to show up
         actionBarDrawerToggle.syncState();
+
+        //set logout item text color to red
+        MenuItem menuItem = mNavigationView.getMenu().findItem(R.id.nav_logout);
+        SpannableString logoutTitle = new SpannableString(menuItem.getTitle());
+        logoutTitle.setSpan(new ForegroundColorSpan(ContextCompat.getColor(this, R.color.nav_logout_text)), 0 , logoutTitle.length(), 0);
+        menuItem.setTitle(logoutTitle);
     }
 
     /***
@@ -365,13 +373,13 @@ public class MainActivity extends AppCompatActivity {
     private Fragment getFragment() {
         switch(navItemIndex) {
             case 0:
-                HomeFragment homeFragment = new HomeFragment();
-                return homeFragment;
+                ListFragment listFragment = new ListFragment();
+                return listFragment;
             case 1:
                 SettingsFragment settingsFragment = new SettingsFragment();
                 return settingsFragment;
             default:
-                return new HomeFragment();
+                return new ListFragment();
         }
     }
 
