c9f2cb7d248199bac7b85add0ab76d34e0a608b2
ryubears <ryubears@gmail.com>
2017-08-09 14:40:42 +0900

    Added License

--- app/src/main/java/com/example/android/socialweather/DetailsActivity.java
+++ app/src/main/java/com/example/android/socialweather/DetailsActivity.java
@@ -17,6 +17,22 @@ import com.example.android.socialweather.utils.WeatherUtils;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+/**
+ * Copyright 2017 Yehyun Ryu
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 public class DetailsActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
     @BindView(R.id.details_description) TextView mDescriptionTextView;
     @BindView(R.id.details_location) TextView mLocationTextView;
--- app/src/main/java/com/example/android/socialweather/HomeFragment.java
+++ app/src/main/java/com/example/android/socialweather/HomeFragment.java
@@ -47,6 +47,22 @@ import java.util.concurrent.TimeUnit;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+/**
+ * Copyright 2017 Yehyun Ryu
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 public class HomeFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor>, SwipeRefreshLayout.OnRefreshListener {
     @BindView(R.id.home_swipe_refresh_layout) SwipeRefreshLayout mHomeSwipeRefreshLayout;
     @BindView(R.id.home_recycler_view) RecyclerView mHomeRecyclerView;
